4e71a57b016c5bc33cb250c3c50e8a7cd7ba836f
fabianx02 <fabianzj@gmail.com>
2021-11-04 22:23:37 -0500

    carrito

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 //import TitleComponent from './components/catalogo/title/title';
 import NavBar from './components/NavBar';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       {/* <NavBar /> */}
       <NavBar/>
+      <Card/>
+      <Card/>
+      <Card/>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,6 +15,7 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 
 const Search = styled('div')(({ theme }) => ({
@@ -163,7 +164,7 @@ export default function PrimarySearchAppBar() {
       <MenuItem>
         <IconButton size="large" aria-label="show 4 new mails" color="inherit">
           <Badge badgeContent={4} color="error">
-            <MailIcon />
+            <hoppingCartIcon/>
           </Badge>
         </IconButton>
         <p>Messages</p>
@@ -231,17 +232,8 @@ export default function PrimarySearchAppBar() {
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
             <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
+              <Badge badgeContent={1} color="error">
+                <ShoppingCartIcon />
               </Badge>
             </IconButton>
             <IconButton
